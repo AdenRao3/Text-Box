b7d9dbde4d7e8354e01b415040e3eaf786cac18d
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-02-14 06:09:54 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+--This program puts a text box on the screen and when you touch a ball it will print the text to the console.
+--
+--Created By: Aden Rao
+--Created On: 2019/02/13
+--
+-----------------------------------------------------------------------------------------
+display.setDefault( "background", 0, 255, 0 )
+
+local answerTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 250, 350, 75 )
+answerTextField.id = "answer textField"
+
+local enterButton = display.newImageRect( "assets/ball.png", 250, 250 )
+enterButton.x = display.contentCenterX
+enterButton.y = display.contentCenterY
+enterButton.id = "enter button"
+ 
+local function enterButtonTouch( event )
+
+    print( answerTextField.text )
+
+    return true
+end
+
+enterButton:addEventListener( "touch", enterButtonTouch )
